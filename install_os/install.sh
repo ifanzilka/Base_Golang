dc14a6cbb9fa4f880f3fa6abcae8e96ded8188b7
Fanzil Nabiyarov <ifanzilka@pop-os.localdomain>
2022-09-03 22:34:08 +0500

    add install linux

--- install_os/install.sh
+++ install_os/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
         echo "This is Linux"
+	sudo apt update -y
+	sudo apt upgrade -y
+	sudo apt install -y golang
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "This is MacOS"
 	brew install go
